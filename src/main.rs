846616c0014d84013fd68b2835a79881bd64c759
davidbruce <bruce.j.david@gmail.com>
2025-05-11 21:40:44 -0400

    Use visitor pattern provided by ast_vist with oxc full features

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
-use oxc::ast::ast::{Statement, TSSignature, TSType};
+use oxc::ast::ast::{Statement, TSSignature, TSType, TSTypeParameter};
 use std::{fs, path::Path};
 use oxc::allocator::Allocator;
 use oxc::parser::{ParseOptions, Parser};
 use oxc::span::SourceType;
+use oxc::semantic::ScopeFlags;
+
+use oxc::ast::ast::*;
+
+use oxc::ast_visit::{Visit, walk};
+
+struct PrintVisitor;
+
+impl<'a> Visit<'a> for PrintVisitor {
+    fn visit_ts_interface_declaration(&mut self, it: &TSInterfaceDeclaration<'a>) {
+        println!("Interface: {}", it.id.name);
+        walk::walk_ts_interface_body(self, &it.body);
+    }
+    fn visit_identifier_name(&mut self, identifier: &IdentifierName<'a>) {
+        println!("\tIdentifier: {}", identifier.name);
+    }
+    fn visit_ts_type(&mut self, ts_type: &TSType<'a>) {
+        println!("\t\tType: {}", process_type(ts_type).unwrap());
+    }
+}
 
 fn process_type(ts_type: &TSType) -> Result<String, String> {
     let result = match ts_type {
@@ -31,6 +51,10 @@ fn process_type(ts_type: &TSType) -> Result<String, String> {
         Ok(result.to_string())
     }
 }
+
+// fn process_type_parameters(param: &TSTypeParameter) -> Result<String, String> {
+//
+// }
 
 fn process_signature(signature: &TSSignature) -> Result<String, String> {
     match signature {
@@ -76,18 +100,21 @@ fn main() -> Result<(), String> {
         .with_options(ParseOptions { parse_regular_expression: true, ..ParseOptions::default() })
         .parse();
 
-    if ret.errors.is_empty() {
-        println!("Parsed Successfully.");
-        // println!("AST:\n{:#?}", ret.program); // Pr
-        for stmt in &ret.program.body {
-            println!("Statement: {}\n\n\n", process_statement(stmt).unwrap());
-        }
-    } else {
-        for error in ret.errors {
-            println!("{:?}", error);
-        }
-        println!("Parsed with Errors.");
-    }
+    let mut visitor = PrintVisitor;
+    visitor.visit_program(&ret.program);
+    // ret.program(&mut visitor); //
+    // if ret.errors.is_empty() {
+    //     println!("Parsed Successfully.");
+    //     // println!("AST:\n{:#?}", ret.program); // Pr
+    //     for stmt in &ret.program.body {
+    //         println!("Statement: {}\n\n\n", process_statement(stmt).unwrap());
+    //     }
+    // } else {
+    //     for error in ret.errors {
+    //         println!("{:?}", error);
+    //     }
+    //     println!("Parsed with Errors.");
+    // }
 
     Ok(())
 }
